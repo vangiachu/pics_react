756586a74b703ace807352e53f8a1d372b550eeb
Chu Văn Giá <gia.chuvan@ncc.asia>
2023-06-21 11:41:39 +0700

    commit sang 21.6.2023

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import searchImages from './api';
+import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList'
+
 function App() {
-  return <div>App</div>;
+  const [images, setImages] = useState([]);
+
+	const handleSubmit = async (term) => {
+		const result = await searchImages(term);
+		setImages(result);
+	};
+
+	return (
+		<div>
+			<SearchBar onSubmit={handleSubmit} />
+      <ImageList images={images} />
+		</div>
+	);
 }
 
 export default App;
